1ee0f6cf2c4e2c926236ab8b286389cb867fdc83
Maur-pixel <mauriciofsilva01@gmail.com>
2024-05-21 08:51:11 -0300

    /

--- Assets/script/MenuPrincipalManager.cs
+++ Assets/script/MenuPrincipalManager.cs
@@ -2,26 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MenuPrincipalManager : MonoBehaviour{
 
     [SerializeField] private string nomeDoLevelDeJogo;
     [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelOpcoes;
+    [SerializeField] private GameObject opcoesAbertoBotao, opcoesFechadoBotao;
 
-   public void Jogar(){
+    public void Jogar(){
     SceneManager.LoadScene(nomeDoLevelDeJogo);
    }
 
    public void AbrirOpcoes(){
     painelMenuInicial.SetActive(false);
     painelOpcoes.SetActive(true);
-   }
+    EventSystem.current.SetSelectedGameObject(null);
+    EventSystem.current.SetSelectedGameObject(opcoesAbertoBotao);
+    }
 
    public void FecharOpcoes(){
     painelOpcoes.SetActive(false);
     painelMenuInicial.SetActive(true);
-   }
+    EventSystem.current.SetSelectedGameObject(null);
+    EventSystem.current.SetSelectedGameObject(opcoesFechadoBotao);
+    }
 
    public void SairJogo(){
     Debug.Log("Sair do jogo");
